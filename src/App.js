4dfa488c1ec4bd26bb725385f119969e04842aac
charishma184507 <charishmareddykallam17@gmail.com>
2025-09-24 11:58:00 +0530

    Fix GitHub Actions deploy path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -8,17 +8,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-      <footer className="app-footer">
-        <p>© 2025 Hotel Booking Platform. All rights reserved.</p>
-      </footer>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+        <footer className="app-footer">
+          <p>© 2025 Hotel Booking Platform. All rights reserved.</p>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
